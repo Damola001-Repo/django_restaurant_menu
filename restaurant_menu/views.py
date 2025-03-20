e4dbb2de1c180421d65f4d8f5fde9bf7b9cfeaca
Surface <surface@gmail.com>
2025-03-20 03:29:28 -0700

    Django context demo

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Item
+from .models import Item, CATEGORIES
 
 
 class MenuList(generic.ListView):
@@ -9,7 +9,8 @@ class MenuList(generic.ListView):
 
 
     def get_context_data(self, *, object_list=None, **kwargs):
-        context = {"meals": "Pizza"}
+        context = super().get_context_data(**kwargs)
+        context["meals"] = CATEGORIES
         return context
 
 
